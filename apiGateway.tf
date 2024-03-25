c43181d0949c58e0c93d81c0bf5ad7c199a80270
Guilherme <guilherme_gon@live.com>
2024-03-25 01:01:38 -0300

    terraform

--- apiGateway.tf
+++ apiGateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "aws_api_gateway_rest_api" {
 resource "aws_api_gateway_resource" "aws_api_gateway_resource" {
   rest_api_id = aws_api_gateway_rest_api.aws_api_gateway_rest_api.id
   parent_id   = aws_api_gateway_rest_api.aws_api_gateway_rest_api.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "registro-ponto"
 }
 
 resource "aws_api_gateway_method" "aws_api_gateway_method" {
@@ -22,7 +22,7 @@ resource "aws_api_gateway_integration" "aws_api_gateway_integration" {
   http_method             = aws_api_gateway_method.aws_api_gateway_method.http_method
   type                    = "HTTP_PROXY"
   integration_http_method = "ANY"
-  uri                     = "http://${aws_lb.ecs_alb.dns_name}/{proxy}"
+  uri                     = "http://${aws_lb.ecs_alb.dns_name}"
   passthrough_behavior    = "WHEN_NO_MATCH"
 }
 
