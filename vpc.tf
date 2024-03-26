f3d88dc3f30b839f637385cc74f5022115c849d6
Guilherme <guilherme_gon@live.com>
2024-03-25 21:03:46 -0300

    terraform api gateway

--- vpc.tf
+++ vpc.tf
@@ -11,30 +11,30 @@ data "aws_availability_zones" "available" {
 
 
 ## Create var.az_count private subnets, each in a different AZ
+resource "aws_subnet" "registro-ponto-private-subnet" {
+ count             = 2
+ cidr_block        = "${cidrsubnet(aws_vpc.registro-ponto-vpc.cidr_block, 8, count.index)}"
+ availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+ vpc_id            =  aws_vpc.registro-ponto-vpc.id
+}
+
+# Create var.az_count public subnets, each in a different AZ
+resource "aws_subnet" "registro-ponto-public-subnet" {
+ count                   = 2
+ cidr_block              = "${cidrsubnet(aws_vpc.registro-ponto-vpc.cidr_block, 8, 2 + count.index)}"
+ availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+ vpc_id                  = "${aws_vpc.registro-ponto-vpc.id}"
+ map_public_ip_on_launch = true
+}
+#
 #resource "aws_subnet" "registro-ponto-private-subnet" {
-# count             = 2
-# cidr_block        = "${cidrsubnet(aws_vpc.registro-ponto-vpc.cidr_block, 8, count.index)}"
-# availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-# vpc_id            =  aws_vpc.registro-ponto-vpc.id
+# vpc_id     = aws_vpc.registro-ponto-vpc.id
+# cidr_block = "10.0.1.0/24"
+# availability_zone       = "us-east-2"
 #}
 #
-## Create var.az_count public subnets, each in a different AZ
 #resource "aws_subnet" "registro-ponto-public-subnet" {
-# count                   = 2
-# cidr_block              = "${cidrsubnet(aws_vpc.registro-ponto-vpc.cidr_block, 8, 2 + count.index)}"
-# availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
-# vpc_id                  = "${aws_vpc.registro-ponto-vpc.id}"
-# map_public_ip_on_launch = true
+# vpc_id     = aws_vpc.registro-ponto-vpc.id
+# cidr_block = "10.0.2.0/24"
+# availability_zone       = "us-east-2"
 #}
-
-resource "aws_subnet" "registro-ponto-private-subnet" {
- vpc_id     = aws_vpc.registro-ponto-vpc.id
- cidr_block = "10.0.1.0/24"
- availability_zone       = "us-east-2"
-}
-
-resource "aws_subnet" "registro-ponto-public-subnet" {
- vpc_id     = aws_vpc.registro-ponto-vpc.id
- cidr_block = "10.0.2.0/24"
- availability_zone       = "us-east-2"
-}
